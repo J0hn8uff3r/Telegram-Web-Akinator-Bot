7ecb945797015aabcf2d2225e5366ae958fc89d4
J0hn8uff3r <bufferoverflow@hotmail.es>
2018-02-08 04:42:36 +0100

    Add files via upload

---
+++ Telegram_Web_Akinator_Bot_Chrome.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Akinator Bot
+// @namespace   Akinator Bot
+// @description This bot let the user to play with another one via Telegram Web and guess the character that the other human being is thinking about
+// @include     http*://web.telegram.org/#/im?p=@*
+// @include     http*://web.telegram.org/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(window).on("load", function() {
+
+    var script2 = document.createElement( 'script' );
+    script2.src = 'https://rawgit.com/J0hn8uff3r/Telegram-Web-Akinator-Bot/master/apinator.js';
+    $("body").append( script2 );
+
+    var script3 = document.createElement( 'script' );
+    script3.src = 'https://rawgit.com/J0hn8uff3r/Telegram-Web-Akinator-Bot/master/script.js';
+    $("body").append( script3 );
+
+    $( ".im_send_buttons_wrap" ).append( '<img src="https://i.snag.gy/8NjKnH.jpg" onclick="start();" >' );
+});
---
+++ Telegram_Web_Akinator_Bot_Firefox.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Akinator Bot
+// @namespace   Akinator Bot
+// @require     https://code.jquery.com/jquery-3.3.1.js
+// @description This bot let the user to play with another one via Telegram Web and guess the character that the other human being is thinking about
+// @include     http*://web.telegram.org/#/im?p=@*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+$(window).on("load", function() {
+    var script = document.createElement( 'script' );
+    script.src = 'https://rawgit.com/J0hn8uff3r/Telegram-Web-Akinator-Bot/master/script.js';
+    $("body").append( script );
+
+    var script2 = document.createElement( 'script' );
+    script2.src = 'https://rawgit.com/J0hn8uff3r/Telegram-Web-Akinator-Bot/master/apinator.js';
+    $("body").append( script2 );
+
+    $( ".im_send_buttons_wrap" ).append( "<img src='https://i.snag.gy/8NjKnH.jpg' onclick='start();' >" );
+});
